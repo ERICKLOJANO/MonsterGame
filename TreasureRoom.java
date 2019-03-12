fd6c7a0616ee597bc7f716bdbfb7c16ed1309939
ERICKLOJANO <ErickJLojano@gmail.com>
2019-03-12 14:31:36 -0400

    Add files via upload

--- TreasureRoom.java
+++ TreasureRoom.java
@@ -1,5 +1,25 @@
 package gameinterfaces;
 
-public class TreasureRoom {
+public class TreasureRoom extends Room {
+	
+	public String Treasure;
+	
+	public TreasureRoom(String description, Monster monster, String Treasure) {
+		super(2, Treasure, monster);
+		description = "";
+		this.monster = monster;
+		Treasure = "";
+	}
+	
+	public void enter(Player player) {
+		System.out.println("This room is " + this.description + " and has this " + this.monster);
+		if(monster.isAlive() == true) {
+			player.room.enter(player);
+		}
+		else {
+			System.out.println("You explored the room and found the hidden treasure!");
+		}
+	}
 
+	
 }
